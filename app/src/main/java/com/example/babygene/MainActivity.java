49fdb1d73269021ea046d57c0b98743818290386
chenheyubetty <heyuc2@illinois.edu>
2019-04-29 19:28:19 -0500

    worked api

--- app/src/main/java/com/example/babygene/MainActivity.java
+++ app/src/main/java/com/example/babygene/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
         generate();
         birthday();
     }
